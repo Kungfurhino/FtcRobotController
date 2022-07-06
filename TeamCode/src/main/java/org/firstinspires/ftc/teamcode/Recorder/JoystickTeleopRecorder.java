d659a04bfde359c366ad1de3a504106a40af539d
Benks <Benkszeto@gmail.com>
2022-07-05 20:36:18 -0400

    Initial commit with roadrunner shell

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Recorder/JoystickTeleopRecorder.java
@@ -0,0 +1,47 @@
+package org.firstinspires.ftc.teamcode.Recorder;
+
+import android.os.Build;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.Range;
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import java.util.Arrays;
+
+import androidx.annotation.RequiresApi;
+
+@TeleOp
+public class JoystickTeleopRecorder extends LinearOpMode {
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    public void runOpMode() throws InterruptedException {
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+        JoystickRecorder recorder = new JoystickRecorder("SlowBack" + ".txt", telemetry);
+
+        waitForStart();
+        recorder.start();
+        while (!isStopRequested()) {
+            double throttle = -gamepad1.right_stick_x;
+            double direction = -gamepad1.left_stick_y;
+            double strafe = -gamepad1.left_stick_x;
+
+            double FR = throttle + direction + strafe;
+            double FL = -throttle + direction - strafe;
+            double BR = throttle + direction - strafe;
+            double BL = -throttle + direction + strafe;
+
+            FR = Range.clip(FR, -0.6, 0.6);
+            FL = Range.clip(FL, -0.6, 0.6);
+            BR = Range.clip(BR, -0.6, 0.6);
+            BL = Range.clip(BL, -0.6, 0.6);
+
+            drive.setMotorPowers(FL, BL, BR, FR);
+            recorder.collectSample(Arrays.asList(direction, strafe, throttle));
+
+            if(gamepad1.a){
+                recorder.stop();
+                break;
+            }
+        }
+    }
+}
